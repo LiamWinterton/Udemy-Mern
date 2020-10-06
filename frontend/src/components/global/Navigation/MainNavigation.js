198cf6b063f8bdfbd83c388568ae747831717582
Liam Winterton <liam.winterton.dev@gmail.com>
2020-10-06 22:15:08 +0100

    Added a react portal to render content outside of the root div

--- frontend/src/components/global/Navigation/MainNavigation.js
+++ frontend/src/components/global/Navigation/MainNavigation.js
@@ -3,11 +3,18 @@ import { Link } from "react-router-dom"
 
 import MainHeader from "./MainHeader"
 import NavLinks from "./NavLinks"
+import SideDrawer from "./SideDrawer"
 
 import "./MainNavigation.css"
 
 const MainNavigation = () => {
 	return (
+		<>
+		<SideDrawer>
+			<nav className="main-navigation__drawer-nav">
+				<NavLinks />
+			</nav>
+		</SideDrawer>
 		<MainHeader>
 			<button className="main-navigation__menu-btn">
 				<span />
@@ -17,10 +24,11 @@ const MainNavigation = () => {
 			<h1 className="main-navigation__title">
 				<Link to="/">My App</Link>
 			</h1>
-			<nav>
+			<nav className="main-navigation__header-nav">
 				<NavLinks />
 			</nav>
 		</MainHeader>
+		</>
 	)
 }
 
